133eaa7b832f45cc6245556151317ffb64cdb5a9
mdwahiduzzaman124 <www.mdwahiduzzaman124@gmail.com>
2024-02-20 22:11:48 +0600

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
